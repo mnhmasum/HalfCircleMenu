18f9a64d9a087e9b7ff93f0a57636c45976180d7
Masum <mnhmasum@gmail.com>
2019-07-03 19:57:19 +0600

    Change default activity

--- app/src/main/java/com/nhasan/circlemenu/PieActivity.java
+++ app/src/main/java/com/nhasan/circlemenu/PieActivity.java
@@ -14,6 +14,7 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.SeekBar;
+import android.widget.TextView;
 
 /**
  * @author Nazmul Hasan Masum
